022c0244aaa585cf8f4b1c229b89d1bc782dfd83
William Dunn <pikablu112@gmail.com>
2019-07-14 14:08:44 -0700

    Finished section 1 of JS Code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -53,4 +53,54 @@ const questions = {
         },
         correctAnswer: "b"
     },
+    {
+        question: "What is the basic plot summary for Fallout: New Vegas?",
+        answers: {
+            a: "You happily live in a Vault, until one day your father leaves and throws the Vault into a state of panic. You follow his trail, hoping to find answers about the past.",
+            b: "You take refuge in a Vault from the nuclear onslaught outside, but you're cryogenically frozen in a sick experiment run by Vault-Tec. You wake up to find your infant son missing and your spouse dead. You set out on a quest to find them.",
+            c: "You leave your Vault to find a replacement water purification chip before your people run out of clean drinking water. Along the way, you uncover a sinister plan to assimilate all human life into a 'grand plan'.",
+            d: "You are a courier, on your way to make an important delivery, when you are shot in the head. You're dug up and nursed back to health, and begin your quest for vengeance and answers."
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Who are the two warring factions in The Elder Scrolls V: Skyrim?",
+        answers: {
+            a: "The Stormcloaks and the Aldmeris Dominion",
+            b: "The Empire and the Aldmeris Dominion",
+            c: "The Empire and the Stormcloaks",
+            d: "The Empire, the Stormcloaks, and the Aldmeris Dominion"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "In 2003, what company accquired the rights to the Fallout franchise?",
+        answers: {
+            a: "Black Isle Studios",
+            b: "Obsidian Entertainment",
+            c: "Bethesda Game Studios",
+            d: "BioWare"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What game was developed by BioWare?",
+        answers: {
+            a: "Pillars of Eternity",
+            b: "Assassin's Creed",
+            c: "Dragon's Dogma: Darkness Arisen",
+            d: "Mass Effect"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "In what year was the last game in the S.T.A.L.K.E.R. trilogy released?",
+        answers: {
+            a: "2007",
+            b: "2009",
+            c: "2012",
+            d: "2015"
+        },
+        correctAnswer: "b"
+    },
 ]}
