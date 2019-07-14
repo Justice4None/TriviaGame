020383a3d2b15bbc367c82d08eae25bd4544a5e6
William Dunn <pikablu112@gmail.com>
2019-07-14 13:41:59 -0700

    Started JS Code

---
+++ assets/javascript/app.js
@@ -0,0 +1,56 @@
+// Initialize Variables
+
+/*Trivia Bank */
+
+const questions = {
+    rpgs: [{
+        question: "When was the first Fallout game released?",
+        answers: {
+            a: "1989",
+            b: "1994",
+            c: "1997",
+            d: "2001"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What company created the Elder Scrolls game series?",
+        answers: {
+            a: "Interplay Studios",
+            b: "Obsidian Entertainment",
+            c: "BioWare",
+            d: "Bethesda Game Studios"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "What is the setting of Pillars of Eternity II?",
+        answers: {
+            a: "The Deadfire Archipelago",
+            b: "The Aedyr Empire",
+            c: "Dyrwood",
+            d: "Old Valia"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What is the title held by the protagonist in The Elder Scrolls V: Skyrim?",
+        answers: {
+            a: "Dragonborn",
+            b: "The Champion of Cyrodiil",
+            c: "The Nerevarine",
+            d: "The Hero of Kvatch"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What is the name of your player character in Assassin's Creed?",
+        answers: {
+            a: "Ezio Auditore da Firenze",
+            b: "Altaïr Ibn-LaʼAhad",
+            c: "Aveline de Grandpré",
+            d: "Alexios"
+        },
+        correctAnswer: "b"
+    },
+]}
