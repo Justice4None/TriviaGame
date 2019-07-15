e17976ec0a24685891338ba24133078626abcbba
William Dunn <pikablu112@gmail.com>
2019-07-14 21:19:23 -0700

    Finished JS Code *NOT FUNCTIONAL* and CSS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -309,7 +309,7 @@ const questions = {
        
     ]},
 
-//Variables
+// Variables
 const welcomeDiv = $('#welcome');
 const gameDiv = $('#game');
 const quizThemeDiv = $('#quiz-theme');
@@ -332,7 +332,8 @@ var currentQuestionNumber = 1;
 var playerchoice = '';
 var currentCorrectAnswer = '';
 
-//Functions
+// Functions
+
 resetTimer = () => {
     time = 30;
 }
@@ -368,9 +369,9 @@ startGame = () => {
         window.setTimeout(function () {
             quizThemeDiv.slideDown(1500);
         }, 500)
-    })};
+    });
 
-    //Choose a theme for the questions
+     // Choose a video game for the questions
     /*RPGs*/
     $('#rpgs').on('click', function () {
         build("rpgs");
@@ -378,53 +379,55 @@ startGame = () => {
         window.setTimeout(function () {
             currentQuestionDiv.show(1500);
             choicesDiv.show(1500);
-            timerDiv.show(1500)
+            timerDiv.show(1500);
         }, 1000);
         gameDiv.css({
-            backgroundColor: "darkKhaki"
+            backgroundColor: "goldenrod"
         });
         playGame();
-});
-/*general Games*/
-$('#general').on('click', function () {
-    build("general");
-    quizThemeDiv.slideUp(1000);
-    window.setTimeout(function () {
-        currentQuestionDiv.show(1500);
-        choicesDiv.show(1500);
-        timerDiv.show(1500)
-    }, 1000);
-    gameDiv.css({
-        backgroundColor: "darkKhaki"
     });
-    playGame();
-});
-/*General games*/
-$('#general').on('click', function () {
-    build("general");
-    quizThemeDiv.slideUp(1000);
-    window.setTimeout(function () {
-        currentQuestionDiv.show(1500);
-        choicesDiv.show(1500);
-        timerDiv.show(1500)
-    }, 1000);
-    gameDiv.css({
-        backgroundColor: "darkKhaki"
+
+    /*FPS Games*/
+    $('#fps').on('click', function () {
+        build("fps");
+        quizThemeDiv.slideUp(1000);
+        window.setTimeout(function () {
+            currentQuestionDiv.show(1500);
+            choicesDiv.show(1500);
+            timerDiv.show(1500);
+        }, 1000);
+        gameDiv.css({
+            backgroundColor: "goldenrod"
+        });
+        playGame();
     });
-    playGame();
-});
+
+    /*General games*/
+    $('#general').on('click', function () {
+        build("general");
+        quizThemeDiv.slideUp(1000);
+        window.setTimeout(function () {
+            currentQuestionDiv.show(1500);
+            choicesDiv.show(1500);
+            timerDiv.show(1500);
+        }, 1000);
+        gameDiv.css({
+            backgroundColor: "goldenrod"
+        });
+        playGame();
+    });
 
 // Reset game scores
-reseteGame = () => {
-    correct = 0
-    incorrect = 0
-    unanswered = 0
+resetGame = () => {
+    correct = 0;
+    incorrect = 0;
+    unanswered = 0;
 }
 
 // Build an array with the chosen question theme
 build = (theme) => {
 
-    if (theme === "dk") {
+    if (theme === "rpgs") {
 
         // Build questions array
 
@@ -473,46 +476,46 @@ build = (theme) => {
         correctArray.push(correctAnswer1, correctAnswer2, correctAnswer3, correctAnswer4, correctAnswer5, correctAnswer6, correctAnswer7, correctAnswer8, correctAnswer9, correctAnswer10);
         console.log(correctArray);
 
-    } else if (theme === "general") {
+    } else if (theme === "fps") {
 
         // Build general theme questions and answers arrays
-        question1 = questions.general[0].question;
-        question2 = questions.general[1].question;
-        question3 = questions.general[2].question;
-        question4 = questions.general[3].question;
-        question5 = questions.general[4].question;
-        question6 = questions.general[5].question;
-        question7 = questions.general[6].question;
-        question8 = questions.general[7].question;
-        question9 = questions.general[8].question;
-        question10 = questions.general[9].question;
+        question1 = questions.fps[0].question;
+        question2 = questions.fps[1].question;
+        question3 = questions.fps[2].question;
+        question4 = questions.fps[3].question;
+        question5 = questions.fps[4].question;
+        question6 = questions.fps[5].question;
+        question7 = questions.fps[6].question;
+        question8 = questions.fps[7].question;
+        question9 = questions.fps[8].question;
+        question10 = questions.fps[9].question;
 
         constructedQs.push(question1, question2, question3, question4, question5, question6, question7, question8, question9, question10);
 
-        answers1 = questions.general[0].answers;
-        answers2 = questions.general[1].answers;
-        answers3 = questions.general[2].answers;
-        answers4 = questions.general[3].answers;
-        answers5 = questions.general[4].answers;
-        answers6 = questions.general[5].answers;
-        answers7 = questions.general[6].answers;
-        answers8 = questions.general[7].answers;
-        answers9 = questions.general[8].answers;
-        answers10 = questions.general[9].answers;
+        answers1 = questions.fps[0].answers;
+        answers2 = questions.fps[1].answers;
+        answers3 = questions.fps[2].answers;
+        answers4 = questions.fps[3].answers;
+        answers5 = questions.fps[4].answers;
+        answers6 = questions.fps[5].answers;
+        answers7 = questions.fps[6].answers;
+        answers8 = questions.fps[7].answers;
+        answers9 = questions.fps[8].answers;
+        answers10 = questions.fps[9].answers;
 
         constructedAs.push(answers1, answers2, answers3, answers4, answers5, answers6, answers7, answers8, answers9, answers10);
         console.log(constructedAs);
 
-        correctAnswer1 = questions.general[0].correctAnswer;
-        correctAnswer2 = questions.general[1].correctAnswer;
-        correctAnswer3 = questions.general[2].correctAnswer;
-        correctAnswer4 = questions.general[3].correctAnswer;
-        correctAnswer5 = questions.general[4].correctAnswer;
-        correctAnswer6 = questions.general[5].correctAnswer;
-        correctAnswer7 = questions.general[6].correctAnswer;
-        correctAnswer8 = questions.general[7].correctAnswer;
-        correctAnswer9 = questions.general[8].correctAnswer;
-        correctAnswer10 = questions.general[9].correctAnswer;
+        correctAnswer1 = questions.fps[0].correctAnswer;
+        correctAnswer2 = questions.fps[1].correctAnswer;
+        correctAnswer3 = questions.fps[2].correctAnswer;
+        correctAnswer4 = questions.fps[3].correctAnswer;
+        correctAnswer5 = questions.fps[4].correctAnswer;
+        correctAnswer6 = questions.fps[5].correctAnswer;
+        correctAnswer7 = questions.fps[6].correctAnswer;
+        correctAnswer8 = questions.fps[7].correctAnswer;
+        correctAnswer9 = questions.fps[8].correctAnswer;
+        correctAnswer10 = questions.fps[9].correctAnswer;
 
         correctArray.push(correctAnswer1, correctAnswer2, correctAnswer3, correctAnswer4, correctAnswer5, correctAnswer6, correctAnswer7, correctAnswer8, correctAnswer9, correctAnswer10);
         console.log(correctArray);
@@ -563,4 +566,187 @@ build = (theme) => {
     }
 }
 
+// Based on the currentQuestionNumber, display the corresponding question and answer choices from their respective arrays
+showQuestion = () => {
+    switch (currentQuestionNumber) {
+        case 1:
+            $('#current-question').empty().html(question1);
+            $('#choices').html(
+                '<ul><li class="btn btn-dark-green btn-lg animated tada" id="a">' + constructedAs[0].a + '</li><li class="btn btn-brown animated flash btn-lg" id="b">' + constructedAs[0].b + '</li><li class="btn btn-lg btn-cyan animated jello" id="c">' + constructedAs[0].c + '</li><li class="btn btn-yellow btn-lg animated wobble" id="c">' + constructedAs[0].d +
+                '</li></ul>'
+            );
+            currentCorrectAnswer = correctArray[0];
+            currentQuestionNumber++;
+            console.log(currentQuestionNumber);
+            break;
+        case 2:
+            $('#current-question').empty().html(question2);
+            $('#choices').html(
+                '<ul><li class="btn btn-dark-green btn-lg animated slideInDown" id="a">' + constructedAs[1].a + '</li><li class="btn btn-brown btn-lg animated slideInDown" id="b">' + constructedAs[1].b + '</li><li class="btn btn-cyan btn-lg animated slideInRight" id="c">' + constructedAs[1].c + '</li><li class="btn btn-yellow btn-lg animated slideInLeft" id="d">' + constructedAs[1].d +
+                '</li></ul>'
+            )
+            currentCorrectAnswer = correctArray[1];
+            currentQuestionNumber++;
+            break;
+        case 3:
+            $('#current-question').empty().html(question3);
+            $('#choices').html(
+                '<ul><li class="btn btn-dark-green btn-lg animated rubberBand" id="a">' + constructedAs[2].a + '</li><li class="btn btn-brown btn-lg animated shake" id="b">' + constructedAs[2].b + '</li><li class="btn btn-cyan btn-lg animated headShake" id="c">' + constructedAs[2].c + '</li><li class="btn btn-yellow btn-lg animated bounceIn" id="d">' + constructedAs[2].d +
+                '</li></ul>'
+            );
+            currentCorrectAnswer = correctArray[2];
+            currentQuestionNumber++;
+            break;
+        case 4:
+            $('#current-question').empty().html(question4);
+            $('#choices').html(
+                '<ul><li class="btn btn-dark-green btn-lg animated bounceInDown" id="a">' + constructedAs[3].a + '</li><li class="btn btn-brown btn-lg animated bounceInUp" id="b">' + constructedAs[3].b + '</li><li class="btn btn-cyan btn-lg animated bounceInRight" id="c">' + constructedAs[3].c + '</li><li class="btn btn-yellow btn-lg animated bounceInLeft" id="d">' + constructedAs[3].d +
+                '</li></ul>'
+            );
+            currentCorrectAnswer = correctArray[3];
+            currentQuestionNumber++;
+            break;
+        case 5:
+            $('#current-question').empty().html(question5);
+            $('#choices').html(
+                '<ul><li class="btn btn-dark-green btn-lg animated fadeIn" id="a">' + constructedAs[4].a + '</li><li class="btn btn-brown btn-lg animated fadeInDownBig" id="b">' + constructedAs[4].b + '</li><li class="btn btn-cyan btn-lg animated fadeInUpBig" id="c">' + constructedAs[4].c + '</li><li class="btn btn-yellow btn-lg animated flipInX" id="d">' + constructedAs[4].d +
+                '</li></ul>'
+            );
+            currentCorrectAnswer = correctArray[4];
+            currentQuestionNumber++;
+            break;
+        case 6:
+            $('#current-question').empty().html(question6);
+            $('#choices').html(
+                '<ul><li class="btn btn-dark-green btn-lg animated flipInY" id="a">' + constructedAs[5].a + '</li><li class="btn btn-brown btn-lg animated lightSpeedIn" id="b">' + constructedAs[5].b + '</li><li class="btn btn-cyan btn-lg animated fadeIn" id="c">' + constructedAs[5].c + '</li><li class="btn btn-yellow btn-lg animated rotateIn" id="d">' + constructedAs[5].d +
+                '</li></ul>'
+            );
+            currentCorrectAnswer = correctArray[5];
+            currentQuestionNumber++;
+            break;
+        case 7:
+            $('#current-question').empty().html(question7);
+            $('#choices').html(
+                '<ul><li class="btn btn-dark-green btn-lg animated rotateInDownLeft" id="a">' + constructedAs[6].a + '</li><li class="btn btn-brown btn-lg animated rotateInDownRight" id="b">' + constructedAs[6].b + '</li><li class="btn btn-cyan btn-lg animated rotateInUpLeft" id="c">' + constructedAs[6].c + '</li><li class="btn btn-lg btn-yellow animated rotateInUpRight" id="d">' + constructedAs[6].d +
+                '</li></ul>'
+            );
+            currentCorrectAnswer = correctArray[6];
+            currentQuestionNumber++;
+            break;
+        case 8:
+            $('#current-question').empty().html(question8);
+            $('#choices').html(
+                '<ul><li class="btn btn-lg btn-dark-green animated rollIn" id="a">' + constructedAs[7].a + '</li><li class="btn btn-brown btn-lg animated zoomIn" id="b">' + constructedAs[7].b + '</li><li class="btn btn-lg btn-cyan animated zoomInDown" id="c">' + constructedAs[7].c + '</li><li class="btn btn-yellow btn-lg animated zoomInRight" id="d">' + constructedAs[7].d +
+                '</li></ul>'
+            );
+            currentQuestionNumber++;
+            break;
+        case 9:
+            $('#current-question').empty().html(question9);
+            $('#choices').html(
+                '<ul><li class="btn btn-dark-green btn-lg animated rollIn" id="a">' + constructedAs[8].a + '</li><li class="btn btn-brown btn-lg animated zoomInLeft" id="b">' + constructedAs[8].b + '</li><li class="btn btn-cyan btn-lg animated tada" id="c">' + constructedAs[8].c + '</li><li class="btn btn-lg btn-yellow animated flipInX" id="d">' + constructedAs[8].d +
+                '</li></ul>'
+            );
+            currentCorrectAnswer = correctArray[8];
+            currentQuestionNumber++;
+            break;
+        case 10:
+            $('#current-question').empty().html(question10);
+            $('#choices').html(
+                '<ul><li><button class="btn btn-lg btn-dark-green animated rotateIn" id="a" type="button">' + constructedAs[9].a + '</button></li><li><button class="btn btn-brown btn-lg animated slideIn" id="b">' + constructedAs[9].b + '</button></li><li><button class="btn btn-cyan btn-lg animated jello" id="c">' + constructedAs[9].c + '</button></li><li> <button class="btn btn-yellow btn-lg animated wobble" id="d">' + constructedAs[9].d +
+                '</button></li></ul>'
+            );
+            currentCorrectAnswer = correctArray[9];
+            currentQuestionNumber++;
+            break;
+    }
 
+    // When an answer is clicked, disable the other buttons and run compareAnswers()
+    $('#a, #b, #c, #d').click(function () {
+        if (this.id == 'a') {
+            stopTimer();
+            $('#b', '#c', '#d').addClass('disabled');
+            playerChoice = 'a';
+            compareAnswers(playerChoice);
+        } else if (this.id == 'b') {
+            stopTimer();
+            $('#a', '#c', '#d').addClass('disabled');
+            playerChoice = 'b';
+            console.log(playerChoice);
+            compareAnswers(playerChoice);
+        } else if (this.id == 'c') {
+            stopTimer();
+            $('#a', '#b', '#d').addClass('disabled');
+            playerChoice = 'c';
+            console.log(playerChoice);
+            compareAnswers(playerChoice);
+        } else if (this.id == 'd') {
+            stopTimer();
+            $('#a', '#b', '#c').addClass('disabled');
+            playerChoice = 'd';
+            console.log(playerChoice);
+            compareAnswers(playerChoice);
+
+        }
+    });
+};
+
+// Check if the playerChoice was correct
+compareAnswers = (playerChoice) => {
+
+    console.log("Comparing player choice");
+    // If correct, add a point to 'correct' and alert
+    if (playerChoice == currentCorrectAnswer) {
+        correct++;
+        alert("Correct Answer!");
+    }
+    // Otherwise add a point to 'incorrect' and alert
+    else {
+        incorrect++;
+        alert("Incorrect Answer...");
+    }
+
+    // Clear out divs to make room for the next question
+    clearDivs();
+}
+
+// Clears out all divs and starts the next question
+clearDivs = () => {
+    timerDiv.empty();
+    currentQuestionDiv.empty();
+    choicesDiv.empty();
+    choicesDiv.html('<ul></ul>');
+    playGame();
+
+}
+
+// Check the currentQuestionNumber and run showQuestion; reset the timer
+playGame = () => {
+    if (currentQuestionNumber < 10) {
+        showQuestion(currentQuestionNumber);
+        resetTimer();
+        startTimer();
+    } else {
+        $('#current-question').empty().html(
+            '<span class="p-2 display-1">End</span><br><h3 class="display-3">Your scores:</h3><br>Correct: ' + correct +
+            '<br>Incorrect: ' + incorrect +
+            '<br>Unanswered: ' + unanswered
+        );
+
+        $('#choices').html(
+            '<button class="btn btn-elegant btn-lg" id="restart">Click to Restart</button>'
+        );
+
+        $('#restart').click(function () {
+            resetGame();
+            gameDiv.fadeOut();
+            startButton.show();
+            startGame();
+        });
+    }
+};
+
+// When the document is ready, populate the start button
+$(document).ready(function () {
+    startGame();
+});
